685a3bc1450b976882cbb258da0137c4fc16a4ae
toehlmer <treyoehmler@gmail.com>
2019-12-03 16:18:36 -0500

    compiled unet_5

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-python3 train.py unet_4 0 25 25 32 0.25
-python3 train.py unet_4 25 55 30 32 0.25
-python3 train.py unet_4 55 90 35 32 0.25
-python3 train.py unet_4 90 130 40 32 0.25
-python3 train.py unet_4 130 180 50 32 0.25
+python3 train.py unet_5 0 25 25 32 0.25
+python3 train.py unet_5 25 55 30 32 0.25
+python3 train.py unet_5 55 90 35 32 0.25
+python3 train.py unet_5 90 130 40 32 0.25
+python3 train.py unet_5 130 180 50 32 0.25
 
 

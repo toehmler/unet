375c0bf356c21cb4e5ba1a39b73be9ec8c415585
toehlmer <treyoehmler@gmail.com>
2019-12-02 21:32:31 -0500

    added metrics to test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from keras.models import load_model
 import json
 from utils import *
 from model import *
+from metrics import *
 from tqdm import tqdm
 
 
@@ -35,6 +36,43 @@ if __name__ == "__main__":
 
     pbar.close()
 
-    
+    gt = np.array(gt)
+    pred = np.array(pred)
+
+    dice_whole = DSC_whole(pred, gt)
+    dice_en = DSC_en(pred, gt)
+    dice_core = DSC_core(pred, gt)
+
+    sen_whole = sensitivity_whole(pred, gt)
+    sen_en = sensitivity_en(pred, gt)
+    sen_core = sensitivity_core(pred, gt)
+
+    spec_whole = specificity_whole(pred, gt)
+    spec_en = specificity_en(pred, gt)
+    spec_core = specificity_core(pred, gt)
+ 
+    print("")
+    print("=======================================")
+    print("Dice whole tumor score: {:0.4f}".format(dice_whole)) 
+    print("Dice enhancing tumor score: {:0.4f}".format(dice_en)) 
+    print("Dice core tumor score: {:0.4f}".format(dice_core)) 
+    print("=======================================")
+    print("Sensitivity whole tumor score: {:0.4f}".format(sen_whole)) 
+    print("Sensitivity enhancing tumor score: {:0.4f}".format(sen_en)) 
+    print("Sensitivity core tumor score: {:0.4f}".format(sen_core)) 
+    print("=======================================")
+    print("Specificity whole tumor score: {:0.4f}".format(spec_whole)) 
+    print("Specificity enhancing tumor score: {:0.4f}".format(spec_en)) 
+    print("Specificity core tumor score: {:0.4f}".format(spec_core)) 
+    '''
+    print("=======================================")
+    print("Hausdorff whole tumor score: {:0.4f}".format(haus_whole)) 
+    print("Hausdorff enhancing tumor score: {:0.4f}".format(haus_en)) 
+    print("Hausdorff core tumor score: {:0.4f}".format(haus_core)) 
+    '''
+    print("=======================================\n")
+
+
+   
     
 

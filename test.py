a5f3b8f2ea003e5f3d30dc12f314bec0500912ee
toehlmer <treyoehmler@gmail.com>
2019-12-03 02:19:51 -0500

    new test script

--- test.py
+++ test.py
@@ -19,83 +19,108 @@ if __name__ == "__main__":
     model = load_model("models/{}.h5".format(model_name),
             custom_objects = {"dice_coef_loss" : dice_coef_loss,
                               "dice_coef" : dice_coef})
-    patient = int(input("Patient no: "))
-    path = glob(config['root'] + "/*pat{}*".format(patient))[0]
-    scans = load_scans(path)
-    scans = norm_scans(scans)
 
-    gt = []
-    pred = []
+    dw = 0
+    de = 0
+    dc = 0
 
-    pbar = tqdm(total = scans.shape[0])
-    for slice_no in range(scans.shape[0]):
-        test_slice = scans[slice_no:slice_no+1,:,:,:4]
-        test_label = scans[slice_no:slice_no+1,:,:,4]
-        prediction = model.predict(test_slice, batch_size=32)
-        prediction = prediction[0]
-        prediction = np.around(prediction)
-        prediction = np.argmax(prediction, axis=-1)
-        gt.extend(test_label[0])
-        pred.extend(prediction)
+    patient = 190
+    while patient < 206:
 
-        scan = test_slice[0,:,:,2]
-        tmp_label = test_label[0]
+        path = glob(config['root'] + "/*pat{}*".format(patient))[0]
+        scans = load_scans(path)
+        scans = norm_scans(scans)
 
-        fig = plt.figure(figsize=(15, 10))
-        plt.subplot(131)
-        plt.title('Input')
-        plt.imshow(scan, cmap='gray')
-        plt.subplot(132)
-        plt.title('Ground Truth')
-        plt.imshow(tmp_label,cmap='gray')
-        plt.subplot(133)
-        plt.title('Prediction')
-        plt.imshow(prediction,cmap='gray')
-        plt.savefig('outputs/{}_pat{}_slice{}.png'.format(model_name,patient,slice_no), bbox_inches='tight')
-        plt.close(fig)
+        gt = []
+        pred = []
 
+        pbar = tqdm(total = scans.shape[0])
+        for slice_no in range(scans.shape[0]):
+            test_slice = scans[slice_no:slice_no+1,:,:,:4]
+            test_label = scans[slice_no:slice_no+1,:,:,4]
+            prediction = model.predict(test_slice, batch_size=32)
+            prediction = prediction[0]
+            prediction = np.around(prediction)
+            prediction = np.argmax(prediction, axis=-1)
+            gt.extend(test_label[0])
+            pred.extend(prediction)
 
-        pbar.update(1)
+            '''
+            scan = test_slice[0,:,:,2]
+            tmp_label = test_label[0]
 
-    pbar.close()
-
-    gt = np.array(gt)
-    pred = np.array(pred)
-
-    dice_whole = DSC_whole(pred, gt)
-    dice_en = DSC_en(pred, gt)
-    dice_core = DSC_core(pred, gt)
-
-    sen_whole = sensitivity_whole(pred, gt)
-    sen_en = sensitivity_en(pred, gt)
-    sen_core = sensitivity_core(pred, gt)
-
-    spec_whole = specificity_whole(pred, gt)
-    spec_en = specificity_en(pred, gt)
-    spec_core = specificity_core(pred, gt)
- 
-    print("")
-    print("=======================================")
-    print("Dice whole tumor score: {:0.4f}".format(dice_whole)) 
-    print("Dice enhancing tumor score: {:0.4f}".format(dice_en)) 
-    print("Dice core tumor score: {:0.4f}".format(dice_core)) 
-    print("=======================================")
-    print("Sensitivity whole tumor score: {:0.4f}".format(sen_whole)) 
-    print("Sensitivity enhancing tumor score: {:0.4f}".format(sen_en)) 
-    print("Sensitivity core tumor score: {:0.4f}".format(sen_core)) 
-    print("=======================================")
-    print("Specificity whole tumor score: {:0.4f}".format(spec_whole)) 
-    print("Specificity enhancing tumor score: {:0.4f}".format(spec_en)) 
-    print("Specificity core tumor score: {:0.4f}".format(spec_core)) 
-    '''
-    print("=======================================")
-    print("Hausdorff whole tumor score: {:0.4f}".format(haus_whole)) 
-    print("Hausdorff enhancing tumor score: {:0.4f}".format(haus_en)) 
-    print("Hausdorff core tumor score: {:0.4f}".format(haus_core)) 
-    '''
-    print("=======================================\n")
+            fig = plt.figure(figsize=(15, 10))
+            plt.subplot(131)
+            plt.title('Input')
+            plt.imshow(scan, cmap='gray')
+            plt.subplot(132)
+            plt.title('Ground Truth')
+            plt.imshow(tmp_label,cmap='gray')
+            plt.subplot(133)
+            plt.title('Prediction')
+            plt.imshow(prediction,cmap='gray')
+            plt.savefig('outputs/{}_pat{}_slice{}.png'.format(model_name,patient,slice_no), bbox_inches='tight')
+            plt.close(fig)
+            '''
 
 
-   
+            pbar.update(1)
+
+            pbar.close()
+
+            gt = np.array(gt)
+            pred = np.array(pred)
+
+            dice_whole = DSC_whole(pred, gt)
+            dice_en = DSC_en(pred, gt)
+            dice_core = DSC_core(pred, gt)
+
+            sen_whole = sensitivity_whole(pred, gt)
+            sen_en = sensitivity_en(pred, gt)
+            sen_core = sensitivity_core(pred, gt)
+
+            spec_whole = specificity_whole(pred, gt)
+            spec_en = specificity_en(pred, gt)
+            spec_core = specificity_core(pred, gt)
+         
+            print("=======================================")
+            print("Patient {}".format(patient))
+            print("---------------------------------------")
+            print("Dice whole tumor score: {:0.4f}".format(dice_whole)) 
+            print("Dice enhancing tumor score: {:0.4f}".format(dice_en)) 
+            print("Dice core tumor score: {:0.4f}".format(dice_core)) 
+            print("---------------------------------------")
+            print("Sensitivity whole tumor score: {:0.4f}".format(sen_whole)) 
+            print("Sensitivity enhancing tumor score: {:0.4f}".format(sen_en)) 
+            print("Sensitivity core tumor score: {:0.4f}".format(sen_core)) 
+            print("---------------------------------------")
+            print("Specificity whole tumor score: {:0.4f}".format(spec_whole)) 
+            print("Specificity enhancing tumor score: {:0.4f}".format(spec_en)) 
+            print("Specificity core tumor score: {:0.4f}".format(spec_core)) 
+            print("=======================================")
+
+            dw += dice_whole
+            de += dice_en
+            dc += dice_core
+
+            patient += 1
+        
+        dw_total = dw / 15
+        de_total = de / 15
+        dc_total = dc / 15
+        print("=======================================")
+        print("Summary (Patients 190 - 205)")
+        print("---------------------------------------")
+        print("Dice whole tumor score: {:0.4f}".format(dice_whole)) 
+        print("Dice enhancing tumor score: {:0.4f}".format(dice_en)) 
+        print("Dice core tumor score: {:0.4f}".format(dice_core)) 
+        print("=======================================")
+
+
+
+
+
+
+       
     
 
